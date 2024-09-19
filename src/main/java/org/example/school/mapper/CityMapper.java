581489e63cfccc8d6c360ed0645c9bd15b4ee9ed
HinTion <1256202766@qq.com>
2024-09-19 22:27:49 +0800

    Initial commit

--- src/main/java/org/example/school/mapper/CityMapper.java
+++ src/main/java/org/example/school/mapper/CityMapper.java
@@ -14,4 +14,7 @@ public interface CityMapper {
 
     @Select("select * from city where city_name = #{cityName}")
     City findByCityName(String name);
+
+    @Select("select * from province join  city on  city_province = province_id where city_name like #{cityName}")
+    City findDetailCity(String cityName);
 }
